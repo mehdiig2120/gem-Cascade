b7149c0054054ee6890780beea82fd33f14e31f9
mehdiig2120 <mehdigolestanii27@gmail.com>
2026-06-01 10:46:36 +0430

    making board2

--- board.cpp
+++ board.cpp
@@ -6,10 +6,18 @@ int main(){
 
     array<array<int, 8>, 8> board; // the size of board is 8x8
 
-    for(int i = 1; i <= 8; i++){
-        for(int j = 1; j <= 8; j++){
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
             board[i][j] = 0;
         }
     }
-
+    board[2][2] = 1;
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            
+            cout << " |__ " << board[i][j] << " __| ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
