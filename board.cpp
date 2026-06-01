765a14b61aeaaddfda8b6538d532bbfeb0f02964
mehdiig2120 <mehdigolestanii27@gmail.com>
2026-06-01 11:05:19 +0430

    making board3

--- board.cpp
+++ board.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include <array>
+#include <Windows.h>
 using namespace std;
 
 int main(){
-
+    SetConsoleOutputCP(CP_UTF8);
     array<array<int, 8>, 8> board; // the size of board is 8x8
 
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            board[i][j] = 0;
+    for(int row = 0; row < 8; row++){
+        for(int col = 0; col < 8; col++){
+            board[row][col] = 0;
         }
     }
-    board[2][2] = 1;
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
+    board[2][2] = 4;
+
+    cout << "┌───┬───┬───┬───┬───┬───┬───┬───┐" << endl;
+
+    for(int row = 0; row < 8; row++){
+        for(int col = 0; col < 8; col++){
             
-            cout << " |__ " << board[i][j] << " __| ";
+        cout << "│ " << board[row][col] << " ";
+
         }
-        cout << endl;
+        cout << "│" << endl;
+        
+        if(row < 7){
+            cout << "├───┼───┼───┼───┼───┼───┼───┼───┤" << endl;
+        }
     }
+    cout << "└───┴───┴───┴───┴───┴───┴───┴───┘" << endl;
     return 0;
 }
